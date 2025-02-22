f61c2370cab7c09b96970122dac8dd88c3dd7f27
esul2 <dltmfsus@naver.com>
2025-02-22 20:58:36 +0900

    자동배포 과제 테스트

--- src/main/java/com/koreait/short_url_project_02/ShortUrlProject02Application.java
+++ src/main/java/com/koreait/short_url_project_02/ShortUrlProject02Application.java
@@ -2,8 +2,10 @@ package com.koreait.short_url_project_02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ShortUrlProject02Application {
 
     public static void main(String[] args) {
